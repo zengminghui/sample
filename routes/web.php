b682f6909f70164f23158646f4e29b6d69a628d1
Danny <531194759@qq.com>
2018-11-07 10:46:53 +0000

    Add account activations

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,8 @@ Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
+/** 删除用户 **/
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+/* 邮箱激活 */
+Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
